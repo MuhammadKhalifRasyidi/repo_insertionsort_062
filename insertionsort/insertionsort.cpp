b2732e7297b4c48498935f529767b3e1047d98b9
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-03-24 15:45:31 +0700

    fix yg bener

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -4,13 +4,6 @@ using namespace std;
 int arr[20];
 int n;
 
-
-#include <iostream>
-using namespace std;
-
-int arr[20];
-int n;
-
 void input() {
 	while (true) {
 		cout << "Masukkan jumlah data pada array : ";
