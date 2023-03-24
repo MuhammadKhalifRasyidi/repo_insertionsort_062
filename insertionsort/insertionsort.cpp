5443bbc394d20c18745023f639ee81a81b925875
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-03-24 15:32:10 +0700

    void display

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -47,3 +47,18 @@ void insertionsort() {
 		j = i - 1;
 
 		while (j >= 0 && arr[j] > temp)
+
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+		cout << "\nPass " << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
+	}
+}
+
+void display() {
