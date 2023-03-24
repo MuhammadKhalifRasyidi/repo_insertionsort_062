f608cdb109fc1640da415bfbf0714529f9b18aba
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-03-24 15:19:51 +0700

    while ture

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -3,3 +3,20 @@ using namespace std;
 
 int arr[20];
 int n;
+
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+	}
